210aad7ceb86f540e69e8802c0f64aa0e8e97af8
JnickU <nicknickeado1@gmail.com>
2021-07-26 11:49:02 -0300

    Exercicio concluido de Matriz

---
+++ Matriz/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace Matriz {
+    class Program {
+        static void Main(string[] args) {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++) {
+                string[] vect = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(vect[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (x == mat[i, j]) {
+                        Console.Write("Position ");
+                        Console.WriteLine(i + "," + j);
+                        if (j > 0)
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        if (j < n - 1)
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        if (i > 0)
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        if (i < m - 1)
+                            Console.WriteLine("Down: " + mat[i + 1, j - 1]);
+                    }
+                }
+            }
+        }
+    }
+}
